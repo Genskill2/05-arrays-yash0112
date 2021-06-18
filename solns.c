fc5abf91a4f77a6e0917d5e6d62eaace200e8786
yash0112 <85066851+yash0112@users.noreply.github.com>
2021-06-18 05:59:09 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -30,4 +30,33 @@ int max(int a[], int l)
    
   }
 
+int min(int a[], int l)
+{
+   int m=a[0];
+   for(int i=0; i<l; i++)
+   {
+     if(m>a[i])
+       m=a[i];
+    }
+    
+    return m;
+ }
+ 
+ int main(void){
+ 
+   int n;
+   printf("How many numbers? - ");
+   scanf("%d", &n);
+   int array[n];
+   for(int i=0; i<n; i++){
+   
+    printf("Enter number %d- ",i+1);
+    scanf("%d", &array[i]);
+    }
+   int minimum=min(array, n);
+   
+   printf("Min number is %d\n", minimum);
+   
+  }
+
 
