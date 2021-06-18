9d3eb7368a46d4147588f144146b1edbaf1a20d6
yash0112 <85066851+yash0112@users.noreply.github.com>
2021-06-18 05:35:42 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,32 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+#include<cs50.c>
+
+int max(int a[], int l)
+{
+   int m=a[0];
+   for(int i=0; i<l; i++)
+   {
+     if(m<a[i])
+       m=a[i];
+    }
+    
+    return m;
+ }
+ 
+ int main(void){
+ 
+   int n;
+   n=get_int("How many numbers?-");
+   int array[n];
+   for(int i=0; i<n; i++){
+   
+    array[i]=get_int("Enter number %d- ", i+1);
+    }
+   int maximum=max(array, n);
+   
+   printf("Max number is %d\n", maximum);
+   
+  }
+
+
